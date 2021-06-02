760f4936a03d62f3a5197b234f8988e282385d3c
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-06-02 16:26:47 +0900

    Update WebController.java

--- src/main/java/com/syncross/learningspring/web/WebController.java
+++ src/main/java/com/syncross/learningspring/web/WebController.java
@@ -3,9 +3,9 @@ package com.syncross.learningspring.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // JSON을 반환하는 컨트롤러
 public class WebController {
-    @GetMapping("hello")
+    @GetMapping("hello") // Get 요청을 받을 수 있는 주소
     public String hello() {
         return "Hello World";
     }
