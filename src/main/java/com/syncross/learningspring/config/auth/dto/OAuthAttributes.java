786b95d1f7e7daf135267109991cea58919bbc5d
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-07-11 19:20:51 +0900

    Add Naver Spring Secutiry

--- src/main/java/com/syncross/learningspring/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/syncross/learningspring/config/auth/dto/OAuthAttributes.java
@@ -37,6 +37,9 @@ public class OAuthAttributes {
             String userNameAttributeName,
             Map<String, Object> attributes
     ) {
+        if (registrationId.equals("naver")) {
+            return ofNaver("id", attributes);
+        }
         return OAuthAttributes.ofGoogle(userNameAttributeName, attributes);
     }
     
@@ -48,6 +51,20 @@ public class OAuthAttributes {
                               .email((String) attributes.get("email"))
                               .picture((String) attributes.get("picture"))
                               .attributes(attributes)
+                              .nameAttributeKey(userNameAttributeName)
+                              .build();
+    }
+    
+    private static OAuthAttributes ofNaver(
+            String userNameAttributeName, Map<String, Object> attributes
+    ) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        
+        return OAuthAttributes.builder()
+                              .name((String) response.get("name"))
+                              .email((String) response.get("email"))
+                              .picture((String) response.get("profile_image"))
+                              .attributes(response)
                               .nameAttributeKey(userNameAttributeName)
                               .build();
     }
