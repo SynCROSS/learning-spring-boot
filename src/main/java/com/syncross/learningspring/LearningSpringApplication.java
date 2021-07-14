280ba1aac9fab6857ebded6e0daa53c68e80443b
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-07-14 22:41:16 +0900

    @EnableJpaAuditing requires at least one entity.

--- src/main/java/com/syncross/learningspring/LearningSpringApplication.java
+++ src/main/java/com/syncross/learningspring/LearningSpringApplication.java
@@ -2,9 +2,9 @@ package com.syncross.learningspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class LearningSpringApplication {
 
