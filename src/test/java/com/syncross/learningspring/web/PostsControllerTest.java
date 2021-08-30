2d67291bade75220d45dc304eb7dc281415109a9
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-08-30 22:31:35 +0900

    Delete Lines

--- src/test/java/com/syncross/learningspring/web/PostsControllerTest.java
+++ src/test/java/com/syncross/learningspring/web/PostsControllerTest.java
@@ -25,7 +25,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
