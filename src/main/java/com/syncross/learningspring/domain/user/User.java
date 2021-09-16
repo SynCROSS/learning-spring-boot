b62f1d7d59b756033291d3a2ab15b1633338b0d2
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-09-16 10:36:55 +0900

    Remove Indents In Blank Lines

--- src/main/java/com/syncross/learningspring/domain/user/User.java
+++ src/main/java/com/syncross/learningspring/domain/user/User.java
@@ -14,20 +14,20 @@ public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(nullable = false)
     private String name;
-    
+
     @Column(nullable = false)
     private String email;
-    
+
     @Column
     private String picture;
-    
+
     @Enumerated(EnumType.STRING) // 기본값은 int
     @Column(nullable = false)
     private Role role;
-    
+
     @Builder
     public User(String name, String email, String picture, Role role) {
         this.name    = name;
@@ -35,15 +35,15 @@ public class User extends BaseTimeEntity {
         this.picture = picture;
         this.role    = role;
     }
-    
+
     public User updateUser(String name, String picture) {
         this.name    = name;
         this.picture = picture;
-        
+
         return this;
     }
-    
-    public String getRoleKey(){
+
+    public String getRoleKey() {
         return this.role.getKey();
     }
 }
