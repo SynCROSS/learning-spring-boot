49c47a51dd270709707f1d5aeda00b0b32b819e9
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-09-17 19:58:58 +0900

    Add Double Quote 2 Variables & Update Command Getting JAR_NAME

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> Deploy New Application"
 
-JAR_NAME=$(find $REPOSITORY/ -maxdepth 1 -name "*-SNAPSHOT-*.jar"  | tail -n 1)
+JAR_NAME=$(find $REPOSITORY/ -maxdepth 1 -name "*-SNAPSHOT-*.jar" -and \! -name "*-plain.jar" -exec ls -tr {} + | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 echo "> Grant Execution Privilege To '$JAR_NAME' $"
@@ -22,6 +22,6 @@ chmod +x "$JAR_NAME"
 IDLE_PROFILE=$(find_idle_profile)
 echo "Execute $JAR_NAME With Profile '$IDLE_PROFILE'"
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties, /home/ec-user/app/application-real-db.properties \
-  -Dspring.profiles.active=$IDLE_PROFILE \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-"$IDLE_PROFILE".properties,/home/ec2-user/app/application-oauth.properties, /home/ec-user/app/application-real-db.properties \
+  -Dspring.profiles.active="$IDLE_PROFILE" \
+  "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
