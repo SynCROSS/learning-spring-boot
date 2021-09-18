b1ef46cee3950c3989e202d9f94f78a62831c1bf
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2021-09-18 19:51:20 +0900

    Remove '$' & Add '>' & Add '2'

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,12 +16,12 @@ echo "> Deploy New Application"
 JAR_NAME=$(find $REPOSITORY/ -maxdepth 1 -name "*-SNAPSHOT-*.jar" -and \! -name "*-plain.jar" -exec ls -tr {} + | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
-echo "> Grant Execution Privilege To '$JAR_NAME' $"
+echo "> Grant Execution Privilege To '$JAR_NAME'"
 chmod +x "$JAR_NAME"
 
 IDLE_PROFILE=$(find_idle_profile)
-echo "Execute $JAR_NAME With Profile '$IDLE_PROFILE'"
+echo "> Execute $JAR_NAME With Profile '$IDLE_PROFILE'"
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-"$IDLE_PROFILE".properties,/home/ec2-user/app/application-oauth.properties, /home/ec-user/app/application-real-db.properties \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-"$IDLE_PROFILE".properties,/home/ec2-user/app/application-oauth.properties, /home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active="$IDLE_PROFILE" \
   "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
